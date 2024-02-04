ee459736539386c4dc9868953c1b5975bf9e76f9
Karina <barakhaeva@inbox.ru>
2024-02-04 16:51:58 +0300

    added ui elements (image, label, button) to storyboard

---
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Karina ❦ on 03.02.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func configCell(for cell: ImagesListCell) { }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("проблемка")
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+
