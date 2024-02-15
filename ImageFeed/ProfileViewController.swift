774b022c96b09ab136bf3156d2d7c95d8010d799
Karina <barakhaeva@inbox.ru>
2024-02-15 12:25:33 +0300

    Created ProfileViewController

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
