adcb3b2561faf517dddc771ba56bc2d36dff0538
Karina <barakhaeva@inbox.ru>
2024-02-15 12:33:25 +0300

    Refactoring

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var profileImage: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction private func logoutButton(_ sender: Any) {
+    }
+}
